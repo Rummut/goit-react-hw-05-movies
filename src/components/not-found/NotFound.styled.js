2887be8c5f1f745904d67243fd1c1595b323645e
Rummut <kzantip@gmail.com>
2023-09-11 01:42:34 +0300

    add NotFound

---
+++ src/components/not-found/NotFound.styled.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const Image = styled.img`
+  margin: auto;
+`;
+
+export const ItemReview = styled.p`
+  display: flex;
+  justify-content: center;
+  font-size: 22px;
+  font-weight: 500;
+`;
--- src/components/not-found/Notfound.jsx
+++ src/components/not-found/Notfound.jsx
@@ -1,5 +1,15 @@
+import { Image, ItemReview } from './NotFound.styled';
+import oopsImage from './oops.png';
+
 const NotFound = () => {
-  return console.log('Please reload your page');
+  return (
+    <div>
+      <Image src={oopsImage} alt="oops" width="300" />;
+      <ItemReview>
+        Something went wrong:( Please reload the page or click another page
+      </ItemReview>
+    </div>
+  );
 };
 
 export default NotFound;
